3269cda119d10b7dc3c8c7a8a87e008693cf713b
izaiasrios <izaias.rios@gmail.com>
2025-09-30 22:45:52 -0300

    Aplicado os recursos do ModelForm.

--- app/urls.py
+++ app/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_view
+from cars.views import new_cars_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', cars_view, name='cars_list'),
+    path('new_car/', new_cars_view, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarModelForm
 
 def cars_view(request):
     search = request.GET.get('search', '').strip()
@@ -11,3 +12,16 @@ def cars_view(request):
 
     return render(request, 'cars.html', {'cars': cars})
 
+
+
+
+
+def new_cars_view(request):
+    if request.method == 'POST':
+        new_cars_form = CarModelForm(request.POST, request.FILES)
+        if new_cars_form.is_valid():
+            new_car = new_cars_form.save()
+            return redirect('cars_list')
+    else:
+        new_cars_form = CarModelForm()
+    return render(request, 'new_car.html', {'new_car_form': new_cars_form})
