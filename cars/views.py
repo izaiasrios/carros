647768c9fc9f281a91b518f3e612d4af5b5fb368
Izaias Rios <izaias.rios@gmail.com>
2025-10-13 22:56:55 -0300

    Criado a página CarDetailViewl.

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 class CarsListView(ListView):
     model = Car
@@ -23,3 +23,9 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+    context_object_name = 'car'    
