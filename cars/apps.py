ebd124fe682774710cf236707bebaec951467383
Izaias Rios <izaias.rios@gmail.com>
2025-10-16 12:19:48 -0300

    Indentação do Signals corrigida.

--- cars/apps.py
+++ cars/apps.py
@@ -1,13 +1,9 @@
 from django.apps import AppConfig
 
 
-
 class CarsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cars'
 
-
-
-def ready(self):
-        # Importa o módulo de signals para registrá-los
+    def ready(self):
         import cars.signals
